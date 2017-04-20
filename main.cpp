973c6f58ea4afe0a32c469b1b6acdc63c6ff16ff
khai96_ <hvksmr1996@gmail.com>
2017-04-20 11:43:34 +0700

    Complete

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <streambuf>
+#include <unistd.h>
+
+using namespace std;
+
+auto getfilecontent (string filename) {
+  typedef istreambuf_iterator<char> icsi;
+  ifstream file(filename);
+  return string(icsi(file), icsi());
+}
+
+auto exists (string filename) {
+  return access(filename.c_str(), F_OK) != -1;
+}
+
+string getoutputfilename (string inputfilename, int number = 0) {
+  auto outputfilename = inputfilename + to_string(number);
+  return exists(outputfilename)
+    ? getoutputfilename(inputfilename, number + 1)
+    : outputfilename
+  ;
+}
+
+int main () {
+  cout << ""
+    "Hoạt động:\n"
+    "1. Đọc nội dung file, lưu vào bộ nhớ đệm\n"
+    "2. Tìm tên file\n"
+    "3. Viết nội dung file vào tên mới\n"
+    "4. Kết thúc\n"
+  ;
+
+  string inputfilename;
+  cout << "Nhập tên file: ";
+  getline(cin, inputfilename, '\n');
+
+  auto outputfilename = getoutputfilename(inputfilename);
+  cout << "Đang viết vào file " << outputfilename << endl;
+  ofstream(outputfilename) << getfilecontent(inputfilename);
+  cout << "Đã viết vào file " << outputfilename << endl;
+  return 0;
+}
